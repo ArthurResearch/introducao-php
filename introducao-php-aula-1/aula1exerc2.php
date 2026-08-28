0fbb9b3d3f9e8cc4fe1f1ae2254db6a71108cc3b
Arthur <ac.arthurcardoso@protonmail.com>
2026-08-27 22:47:48 -0300

    Add files via upload

--- introducao-php-aula-1/aula1exerc2.php
+++ introducao-php-aula-1/aula1exerc2.php
@@ -1,9 +1,10 @@
 <?php
 
-$n = 5;
-$r = 3;
+$total = 0;
 
-for ($i = 1; $i <= 10; ++$i) {
-    echo $n . PHP_EOL;
-    $n = $n + $r;
+for ($i = 1; $i <= 100; $i++){
+    echo $i . "\n";
+    $total = $total + $i;
 }
+
+echo $total;
