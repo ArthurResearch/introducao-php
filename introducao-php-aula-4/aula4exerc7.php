5da15944e16c28fe6e521aee042a6a79175eca36
Arthur <ac.arthurcardoso@protonmail.com>
2026-08-28 17:26:14 -0300

    Add files via upload

--- introducao-php-aula-4/aula4exerc7.php
+++ introducao-php-aula-4/aula4exerc7.php
@@ -1,8 +1,22 @@
 <?php
 
 $pessoas = array();
+$dados = array();
 
 for ($i=1; $i<=4; $i++){
-    $pessoa = array();
-    $pessoas ["nome"] => readline("");
+    $dados["nome"] = readline("Informe seu nome: ");
+    $dados["idade"] = readline("Informe sua idade: ");
+    $dados["cidadeNatal"] = readline("Informe a sua cidade natal: ");
+    $dados["profissao"] = readline("Informe a sua profissão: ");
+    array_push($pessoas, $dados);
 }
+
+$pessoaVelha = $pessoas[0];
+
+foreach ($pessoas as $p) {
+    if ($p["idade"] > $pessoaVelha["idade"]) {
+        $pessoaVelha = $p;
+    }
+}
+
+echo "Nome: " . $pessoaVelha["nome"] . " | idade: " . $pessoaVelha["idade"] . " | Cidade Natal: " . $pessoaVelha["cidadeNatal"] . " | Profissão: " . $pessoaVelha["profissao"];
